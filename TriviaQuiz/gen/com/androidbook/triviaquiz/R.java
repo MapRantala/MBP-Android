acb092cb43e1b63755418f0fb86d5ea3d39c5112
Matt Rantala <MapRantala@Gmail.com>
2014-03-02 21:22:47 -0600

    Started

--- TriviaQuiz/gen/com/androidbook/triviaquiz/R.java
+++ TriviaQuiz/gen/com/androidbook/triviaquiz/R.java
@@ -13,18 +13,21 @@ public final class R {
         public static final int fade_in=0x7f040001;
         public static final int fade_in2=0x7f040002;
     }
+    public static final class array {
+        public static final int genders=0x7f070000;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int menu_color=0x7f070003;
-        public static final int menu_glow=0x7f070004;
-        public static final int score_color=0x7f070005;
-        public static final int tab_active=0x7f070006;
-        public static final int tab_inactive=0x7f070007;
-        public static final int tab_text=0x7f070008;
-        public static final int titlecolor=0x7f070000;
-        public static final int versionbackgroundcolor=0x7f070002;
-        public static final int versiontextcolor=0x7f070001;
+        public static final int menu_color=0x7f080003;
+        public static final int menu_glow=0x7f080004;
+        public static final int score_color=0x7f080005;
+        public static final int tab_active=0x7f080006;
+        public static final int tab_inactive=0x7f080007;
+        public static final int tab_text=0x7f080008;
+        public static final int titlecolor=0x7f080000;
+        public static final int versionbackgroundcolor=0x7f080002;
+        public static final int versiontextcolor=0x7f080001;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -33,13 +36,13 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f080000;
-        public static final int activity_vertical_margin=0x7f080001;
-        public static final int menu_item_size=0x7f080005;
-        public static final int score_size=0x7f080006;
-        public static final int splash_fontsize_body=0x7f080003;
-        public static final int splash_fontsize_title=0x7f080002;
-        public static final int splash_spacing=0x7f080004;
+        public static final int activity_horizontal_margin=0x7f090000;
+        public static final int activity_vertical_margin=0x7f090001;
+        public static final int menu_item_size=0x7f090005;
+        public static final int score_size=0x7f090006;
+        public static final int splash_fontsize_body=0x7f090003;
+        public static final int splash_fontsize_title=0x7f090002;
+        public static final int splash_spacing=0x7f090004;
     }
     public static final class drawable {
         public static final int bkgrnd=0x7f020000;
@@ -54,36 +57,46 @@ public final class R {
         public static final int textured=0x7f020009;
     }
     public static final class id {
-        public static final int ImageView_MenuBottom=0x7f0c0007;
-        public static final int ImageView_MenuHeader=0x7f0c0005;
-        public static final int ImageView_MenuHeader2=0x7f0c0003;
-        public static final int ImageView_MenuHeader_Help=0x7f0c0001;
-        public static final int ImageView_MenuHeader_Score=0x7f0c0009;
-        public static final int ListView_Menu=0x7f0c0008;
-        public static final int RelativeLayout01=0x7f0c0000;
-        public static final int RelativeLayout02=0x7f0c0006;
-        public static final int ScrollViewAllScores=0x7f0c000c;
-        public static final int ScrollViewFriendsScores=0x7f0c000e;
-        public static final int Tabhost1=0x7f0c000b;
-        public static final int TableLayout01=0x7f0c0012;
-        public static final int TableLayout_AllScores=0x7f0c000d;
-        public static final int TableLayout_FriendsScores=0x7f0c000f;
-        public static final int TextView01=0x7f0c0002;
-        public static final int TextView01_scores=0x7f0c000a;
-        public static final int TextViewBottomTitle=0x7f0c0019;
-        public static final int TextViewBottomVersion=0x7f0c001a;
-        public static final int TextViewTopTitle=0x7f0c0011;
-        public static final int TextView_HelpText=0x7f0c0004;
-        public static final int action_settings=0x7f0c001d;
-        public static final int help_menu_item=0x7f0c001c;
-        public static final int imageView1=0x7f0c0014;
-        public static final int imageView2=0x7f0c0015;
-        public static final int imageView3=0x7f0c0017;
-        public static final int imageView4=0x7f0c0018;
-        public static final int settings_menu_item=0x7f0c001b;
-        public static final int splash_primarylayout=0x7f0c0010;
-        public static final int tableRow1=0x7f0c0013;
-        public static final int tableRow2=0x7f0c0016;
+        public static final int Button_Password=0x7f0d0018;
+        public static final int EditText_Nickname=0x7f0d0016;
+        public static final int ImageView_MenuBottom=0x7f0d0007;
+        public static final int ImageView_MenuHeader=0x7f0d0005;
+        public static final int ImageView_MenuHeader2=0x7f0d0003;
+        public static final int ImageView_MenuHeader2b=0x7f0d0013;
+        public static final int ImageView_MenuHeader_Help=0x7f0d0001;
+        public static final int ImageView_MenuHeader_Score=0x7f0d0009;
+        public static final int ImageView_MenuHeader_Settings=0x7f0d0011;
+        public static final int ListView_Menu=0x7f0d0008;
+        public static final int NicknameLabel=0x7f0d0015;
+        public static final int PasswordLabel=0x7f0d0017;
+        public static final int PasswordLabel2=0x7f0d0019;
+        public static final int RelativeLayout01=0x7f0d0000;
+        public static final int RelativeLayout02=0x7f0d0006;
+        public static final int RelativeLayout03=0x7f0d0010;
+        public static final int ScrollViewAllScores=0x7f0d000c;
+        public static final int ScrollViewFriendsScores=0x7f0d000e;
+        public static final int Tabhost1=0x7f0d000b;
+        public static final int TableLayout01=0x7f0d001c;
+        public static final int TableLayout_AllScores=0x7f0d000d;
+        public static final int TableLayout_FriendsScores=0x7f0d000f;
+        public static final int TextView01=0x7f0d0002;
+        public static final int TextView013=0x7f0d0012;
+        public static final int TextView01_scores=0x7f0d000a;
+        public static final int TextViewBottomTitle=0x7f0d0023;
+        public static final int TextViewBottomVersion=0x7f0d0024;
+        public static final int TextViewTopTitle=0x7f0d001b;
+        public static final int TextView_HelpText=0x7f0d0004;
+        public static final int action_settings=0x7f0d0027;
+        public static final int help_menu_item=0x7f0d0026;
+        public static final int imageView1=0x7f0d001e;
+        public static final int imageView2=0x7f0d001f;
+        public static final int imageView3=0x7f0d0021;
+        public static final int imageView4=0x7f0d0022;
+        public static final int scrollView1=0x7f0d0014;
+        public static final int settings_menu_item=0x7f0d0025;
+        public static final int splash_primarylayout=0x7f0d001a;
+        public static final int tableRow1=0x7f0d001d;
+        public static final int tableRow2=0x7f0d0020;
     }
     public static final class layout {
         public static final int game=0x7f030000;
@@ -95,37 +108,54 @@ public final class R {
         public static final int splash=0x7f030006;
     }
     public static final class menu {
-        public static final int gameoptions=0x7f0b0000;
-        public static final int quiz_game=0x7f0b0001;
-        public static final int quiz_help=0x7f0b0002;
-        public static final int quiz_menu=0x7f0b0003;
-        public static final int quiz_scores=0x7f0b0004;
-        public static final int quiz_settings=0x7f0b0005;
-        public static final int quiz_splash=0x7f0b0006;
+        public static final int gameoptions=0x7f0c0000;
+        public static final int quiz_game=0x7f0c0001;
+        public static final int quiz_help=0x7f0c0002;
+        public static final int quiz_menu=0x7f0c0003;
+        public static final int quiz_scores=0x7f0c0004;
+        public static final int quiz_settings=0x7f0c0005;
+        public static final int quiz_splash=0x7f0c0006;
     }
     public static final class raw {
         public static final int quizhelp=0x7f060000;
     }
     public static final class string {
-        public static final int all_scores=0x7f09000d;
-        public static final int app_name=0x7f090000;
-        public static final int friends_scores=0x7f09000e;
-        public static final int game=0x7f09000b;
-        public static final int help=0x7f090001;
-        public static final int menu=0x7f090002;
-        public static final int menu_option1_PlayGame=0x7f090003;
-        public static final int menu_option2_ViewScores=0x7f090004;
-        public static final int menu_option3_Settings=0x7f090005;
-        public static final int menu_option4_Help=0x7f090006;
-        public static final int no_scores=0x7f090011;
-        public static final int rank=0x7f090012;
-        public static final int score=0x7f090010;
-        public static final int scores=0x7f09000c;
-        public static final int settings=0x7f09000a;
-        public static final int splash_bottom=0x7f090008;
-        public static final int splash_top=0x7f090007;
-        public static final int splash_version=0x7f090009;
-        public static final int username=0x7f09000f;
+        public static final int all_scores=0x7f0a000d;
+        public static final int app_name=0x7f0a0000;
+        public static final int friends_scores=0x7f0a000e;
+        public static final int game=0x7f0a000b;
+        public static final int gender_female=0x7f0a0021;
+        public static final int gender_male=0x7f0a0020;
+        public static final int gender_neutral=0x7f0a0022;
+        public static final int help=0x7f0a0001;
+        public static final int menu=0x7f0a0002;
+        public static final int menu_option1_PlayGame=0x7f0a0003;
+        public static final int menu_option2_ViewScores=0x7f0a0004;
+        public static final int menu_option3_Settings=0x7f0a0005;
+        public static final int menu_option4_Help=0x7f0a0006;
+        public static final int no_scores=0x7f0a0011;
+        public static final int rank=0x7f0a0012;
+        public static final int score=0x7f0a0010;
+        public static final int scores=0x7f0a000c;
+        public static final int set_password_string=0x7f0a0023;
+        public static final int settings=0x7f0a000a;
+        public static final int settings_button_dob=0x7f0a0017;
+        public static final int settings_button_pwd=0x7f0a0018;
+        public static final int settings_dob=0x7f0a0015;
+        public static final int settings_dob_not_set=0x7f0a0016;
+        public static final int settings_email=0x7f0a0014;
+        public static final int settings_gender=0x7f0a0019;
+        public static final int settings_nickname=0x7f0a0013;
+        public static final int settings_password=0x7f0a001a;
+        public static final int settings_password2=0x7f0a001b;
+        public static final int settings_pwd_equal=0x7f0a001f;
+        public static final int settings_pwd_not_equal=0x7f0a001e;
+        public static final int settings_pwd_not_set=0x7f0a001d;
+        public static final int settings_pwd_set=0x7f0a001c;
+        public static final int splash_bottom=0x7f0a0008;
+        public static final int splash_top=0x7f0a0007;
+        public static final int splash_version=0x7f0a0009;
+        public static final int username=0x7f0a000f;
     }
     public static final class style {
         /** 
@@ -149,11 +179,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f0a0000;
+        public static final int AppBaseTheme=0x7f0b0000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f0a0001;
+        public static final int AppTheme=0x7f0b0001;
     }
     public static final class xml {
         public static final int allscores=0x7f050000;
