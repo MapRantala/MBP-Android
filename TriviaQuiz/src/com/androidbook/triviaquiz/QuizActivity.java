a4bb58666e49be396e7880052752e72ce2d01e82
Matt Rantala <MapRantala@Gmail.com>
2014-03-03 19:39:43 -0600

    Hour 10 Done

--- TriviaQuiz/src/com/androidbook/triviaquiz/QuizActivity.java
+++ TriviaQuiz/src/com/androidbook/triviaquiz/QuizActivity.java
@@ -4,7 +4,12 @@ import android.content.SharedPreferences;
 
 public class QuizActivity extends Activity {
 	public static final String GAME_PREFERENCES = "GamePrefs";
-
+	public static final String GAME_PREFERENCES_NICKNAME = "Nickname";
+	public static final String GAME_PREFERENCES_EMAIL = "Email";
+	public static final String GAME_PREFERENCES_PASSWORD = "Password";
+	public static final String GAME_PREFERENCES_DOB = "DOB";
+	public static final String GAME_PREFERENCES_GENDER = "Gender";
+	
 	protected void setPref(){
 		SharedPreferences settings = getSharedPreferences(GAME_PREFERENCES, MODE_PRIVATE);
 		SharedPreferences.Editor prefEditor = settings.edit();
